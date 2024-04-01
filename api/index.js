357ad3cd88be0144e56ea63e8e885b1be635d91d
Rinku Shivhare <rinku.shatam@gmail.com>
2024-04-01 12:09:51 +0530

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-const app= express();
+dotenv.config();
 
-app.listen(3001,() => {
-    console.log("Server is running at port 3001")
-})
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Mongodb is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const app = express();
+
+app.listen(3001, () => {
+  console.log("Server is running at port 3001");
+});
